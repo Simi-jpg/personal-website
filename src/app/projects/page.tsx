1c8b7a6daac2e262a45388dda596126781980d48
Simi Adeniyi <simiadeniyi97@gmail.com>
2026-04-23 21:05:16 -0500

    Made projects page draft

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import ProjectOrb from "@/components/ProjectOrbs/ProjectOrb";
 
 export default function projects() {
 
@@ -6,11 +6,12 @@ export default function projects() {
   const projects = [
 
     {
-      id: 1,
-      name: "Project 1",
-      tech_stack: ["React", "TypeScript"],
-      summary: "A simple project to showcase my skills.",
-      github: "https://github.com/username/project1",
+      id: 1, name: "Project 1",
+    },
+    {
+      id: 2, name: "Project 2",
+    },
+    {      id: 3, name: "Project 3",
     },
   ];
 
@@ -24,16 +25,7 @@ export default function projects() {
 
       <div className = "flex flex-wrap gap-8 justify-center">
         {projects.map((project) => (
-          <div
-            key={project.id}
-            className="w-40 h-40 rounded-full bg-white/10 backdrop-blur-md border border-white/20
-            shadow-[0_0_25px_rgba(246,193,204,0.25)]
-            flex items-center justify-center text-center p-4"
-          >
-            <span className="text-sm text-[var(--color-black)] font-large">
-              {project.name}
-            </span>
-          </div>
+          <ProjectOrb key={project.id} name={project.name} number={project.id} />
         ))}
       </div>
     </main>
