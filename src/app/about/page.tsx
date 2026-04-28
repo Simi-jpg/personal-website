c44f06cdf7260dbf7215e4522871637437a468b9
Simi Adeniyi <simiadeniyi97@gmail.com>
2026-04-28 14:56:26 -0500

    Added about me content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,20 +7,28 @@ export default function About() {
 
         {/* Left Panel */}
 
-        <div className = "rounded-3xl bg-[var(--color-white)] backdrop-blur-md p-8 shadow-[0_0_120px_rgba(246,193,204,1.0)] min-h-[500px] rotate-[-3deg]">
+        <div className = "rounded-3xl bg-[var(--color-white)] backdrop-blur-md p-8 shadow-[0_0_120px_rgba(246,193,204,1.0)] min-h-[300px] rotate-[-3deg]">
          
           <p>
-            Hi, I&apos;m Simi
+            Hi, I&apos;m Simi. I&apos;m someone who learns by exploring. Instead of committing to one path early, I&apos;ve spent time trying different areas of tech to understand what actually fits me.
+
+            I enjoy when things feel personal. Whether it&apos;s building this website or working on a project, I naturally lean toward creating things that reflect identity rather than just functionality.
+
+            A lot of my ideas come from connecting things I enjoy outside of tech like music, visuals, storytelling, and then bringing that into what I build.
           </p>
           
         </div>
 
         {/* Right Panel */}
 
-        <div className = "rounded-3xl bg-[var(--color-white)] backdrop-blur-md p-8 shadow-[0_0_120px_rgba(140,181,97,1.0)] min-h-[500px] rotate-[4deg]">
+        <div className = "rounded-3xl bg-[var(--color-white)] backdrop-blur-md p-8 shadow-[0_0_120px_rgba(140,181,97,1.0)] min-h-[300px] rotate-[4deg]">
           
           <p>
-            Ohh yeah!!!
+            I&apos;m currently a third-year Computer Science student at the University of Manitoba, actively working toward my first co-op opportunity.
+
+            Through my coursework and projects, I&apos;ve focused on building strong fundamentals in software development. My work on Campus Shenanigans helped me understand how to structure applications using layered architecture, and how to think through both system design and user experience.
+
+            I tend to learn best by building, which has led me to explore different areas of development as I refine where I want to specialize. Right now, I&apos;m especially interested in roles that allow me to contribute to real systems while continuing to grow as a developer.
           </p>
           
         </div>
